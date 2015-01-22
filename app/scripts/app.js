aad0318796cfefc8a5b98022b45a2952aa86eae2
Asher <asher@esc3051-dell-n-provart.eeb.utoronto.ca>
2015-01-22 11:52:16 -0500

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,6 @@
+(function(window, $, undefined) {
+  'use strict';
+
+  console.log('Hello, Processing_test_app!');
+  $('#can').attr('data-processing-sources', 'app/sketches/app.pde');
+})(window, jQuery);
