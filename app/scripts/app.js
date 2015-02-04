337fc32b8a3fe575c2788e46f9a2ca050f994241
Asher <asher.pasha@utoronto.ca>
2015-02-04 17:13:28 -0500

    Fixed RegEx.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,7 +6,7 @@
   var allScripts, i, re, pdeURL;
   
   allScripts = document.querySelectorAll('script');
-  re = /^(.*)(\/bower_components\/Araport_bower_test)\/hello\.js??(.*)?$/;
+  re = /^(.*)(\/Araport_bower_test[^\/]*)\/(.*)hello\.js??(.*)?$/;
   for (i = 0; i < allScripts.length && ! pdeURL; i++) {
     if (re.test(allScripts[i].src)) {	  
       var match = re.exec(allScripts[i].src);
