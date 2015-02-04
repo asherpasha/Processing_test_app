b035d53cc3981f7f2bf3c256a7e2bfa65875b4a2
Asher <asher.pasha@utoronto.ca>
2015-02-04 17:01:12 -0500

    Testing another solution.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,5 +2,19 @@
   'use strict';
 
   console.log('Hello, Processing_test_app!');
-  $('#can').attr('data-processing-sources', 'bower_components/Araport_bower_test/app.pde');
+  // Load the PDE
+  var allScripts, i, re, pdeURL;
+  
+  allScripts = document.querySelectorAll('script');
+  re = /^(.*)(\/bower_components\/Araport_bower_test)\/hello\.js??(.*)?$/;
+  for (i = 0; i < allScripts.length && ! pdeURL; i++) {
+    if (re.test(allScripts[i].src)) {	  
+      var match = re.exec(allScripts[i].src);
+      pdeURL = match[1] + match[2] + '/app.pde';
+    }
+  }
+
+  if (pdeURL) {
+    $('#can').attr('data-processing-sources', pdeURL);
+  }
 })(window, jQuery);
