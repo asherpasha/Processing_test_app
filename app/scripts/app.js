6cf4b5b8373999783581d99862717cdd9416402b
Asher <asher.pasha@utoronto.ca>
2015-02-04 16:22:53 -0500

    Bower test

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,5 +2,5 @@
   'use strict';
 
   console.log('Hello, Processing_test_app!');
-  $('#can').attr('data-processing-sources', 'app/sketches/app.pde');
+  $('#can').attr('data-processing-sources', 'bower_components/Araport_bower_test/app.pde');
 })(window, jQuery);
